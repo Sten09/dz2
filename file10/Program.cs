ef096b5b56e4fede2b8f9e8ac829b9487886f797
Sten09 <sten111982@gmail.com9>
2022-10-07 07:53:36 +0300

    dz2

---
+++ file10/Program.cs
@@ -0,0 +1,10 @@
+﻿/*
+Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+Console.WriteLine("Введите трехзначное число:");
+int threeNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeNumber);
+Console.WriteLine("вторая цифра этого числа "+stringNumber[1]);
---
+++ file15/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+Console.Write("Введи цифру дня недели: ");
+int Numberday = Convert.ToInt32(Console.ReadLine());
+
+void Week (int Numberday) 
+{
+  if (Numberday == 6 || Numberday == 7) 
+    Console.WriteLine("этот день выходной");
+    else if (Numberday < 1 || Numberday > 7) 
+      Console.WriteLine("это не день недели");
+    else Console.WriteLine("этот день не выходной");
+}
+Week(Numberday);
